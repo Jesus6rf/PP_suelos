7a1a781660f6ea0b1fea22daaf7d716c39055874
Jesus6rf <i2025210@continental.edu.pe>
2025-02-27 19:38:28 -0500

    jjjnj

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,5 @@
-import supabase
+import os
+from supabase import create_client
 import pickle
 import requests
 from io import BytesIO
@@ -7,16 +8,16 @@ from io import BytesIO
 SUPABASE_URL = "https://kuztdsenxrumlvwygzdn.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1enRkc2VueHJ1bWx2d3lnemRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2OTI0NjksImV4cCI6MjA1NjI2ODQ2OX0.PhGg9A5k-UUoIc83LhLdETIl1WbUErRMBnzQwkRjlPc"
 
-supabase_client = supabase.create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase_client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# Función para obtener datos de la tabla "suelo_registros"
+# Función para obtener datos de la tabla "suelos_registros"
 def obtener_registros():
-    response = supabase_client.table("suelo_registros").select("*").execute()
+    response = supabase_client.table("suelos_registros").select("*").execute()
     return response.data if response.data else []
 
 # Función para insertar un nuevo registro
 def insertar_registro(datos):
-    response = supabase_client.table("suelo_registros").insert(datos).execute()
+    response = supabase_client.table("suelos_registros").insert(datos).execute()
     return response.data if response.data else None
 
 # Función para leer el modelo directamente desde Supabase Storage sin descargarlo
@@ -24,8 +25,8 @@ def leer_modelo():
     bucket_name = "modelos"
     file_name = "xgboost_multioutput.pkl"
     
-    response = supabase_client.storage.from_(bucket_name).get_public_url(file_name)
+    response = supabase_client.storage.from_(bucket_name).download(file_name)
     if response:
-        return pickle.load(BytesIO(requests.get(response).content))
+        return pickle.load(BytesIO(response))
     else:
         raise Exception("No se pudo leer el modelo desde Supabase.")
